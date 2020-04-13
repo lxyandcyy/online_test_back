b722888bf87d9b0cdc6ab36ee3afeb7cfe418108
lxyandcyy <lxy_from_swust@foxmail.com>
2020-04-13 14:35:14 +0800

    试卷新增、试卷列表

--- routes/paper.js
+++ routes/paper.js
@@ -45,15 +45,17 @@ router.get("/sel-paper", function (req, res) {
     );
 
     for (let q of single_paper_question_custom_answer) {
-      dataValues.questionItems.push({
-        que_id: q.id,
-        que_score: q.question_score,
+      await Module.T_Question.findAll({
+        where: {
+          id: q.question_id,
+        },
+      }).then((res) => {
+        console.log("我是在t_question表中找到的题目：", res);
+        dataValues.questionItems.push(res[0]);
       });
     }
 
     dataValues.pap = single_paper[0];
-
-    console.log(dataValues);
 
     res.json(dataValues);
   })();
@@ -119,26 +121,56 @@ router.post("/edit-paper", function (req, res) {
   let req_body = req.body;
   console.log("编辑题目所提交的数据：", req_body);
 
-  //编辑题目时间 格式处理
+  //编辑试卷时间 格式处理
   let time = new Date(req_body.create_time);
   req_body.create_time = `${time.getFullYear()}-${
     time.getMonth() + 1
   }-${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
 
   //   options格式处理
-  req_body.options = JSON.stringify(req_body.options);
+  req_body.questionItems.options = JSON.stringify(
+    req_body.questionItems.options
+  );
 
   (async () => {
-    let single_que = await Module.T_Question.update(req_body, {
-      where: {
-        id: req_body.id,
+    let single_paper = await Module.T_Exam_Paper.update(
+      {
+        subject_id: req_body.subject_id,
+        name: req_body.name,
+        countdown: req_body.countdown,
+        create_time: req_body.create_time,
       },
-    }); //更新题目数据
-    console.log("编辑题目: " + JSON.stringify(single_que));
+      {
+        where: {
+          id: req_body.id,
+        },
+      }
+    ); //更新t_exam_paper表
+
+    // 删除试卷对应的所有题目
+    let paper_que_cus_ans = await Module.T_Exam_Paper_Question_Custom_Answer.destroy(
+      {
+        where: {
+          exam_paper_id: req_body.id,
+        },
+      }
+    );
+
+    // 添加试卷对应的题目
+    for (let p = 0; p < req_body.questionItems.length; p++) {
+      let paper_que_cus_ans = await Module.T_Exam_Paper_Question_Custom_Answer.create(
+        {
+          exam_paper_id: req_body.id,
+          question_id: req_body.questionItems[p].id,
+          subject_id: req_body.questionItems[p].subject_id,
+        }
+      );
+      console.log("添加试卷: " + JSON.stringify(paper_que_cus_ans));
+    }
 
     res.json({
       state: 200,
-      msg: "编辑题目成功",
+      msg: "编辑试卷成功",
     });
   })();
 });
@@ -148,15 +180,24 @@ router.post("/edit-paper", function (req, res) {
 */
 router.post("/del-paper", function (req, res) {
   let Id = req.body.id;
-  console.log("删除题目所提交的id：", Id);
+  console.log("删除试卷所提交的id：", Id);
 
   (async () => {
-    let single_que = await Module.T_Question.destroy({
+    let single_paper = await Module.T_Exam_Paper.destroy({
       where: {
         id: Id,
       },
     }); //删除题目
-    console.log("删除题目: " + JSON.stringify(single_que));
+
+    let paper_que_cus_ans = await Module.T_Exam_Paper_Question_Custom_Answer.destroy(
+      {
+        where: {
+          exam_paper_id: Id,
+        },
+      }
+    ); //
+    console.log("删除题目: " + JSON.stringify(single_paper));
+    console.log("删除题目: " + JSON.stringify(paper_que_cus_ans));
 
     res.json({
       state: 200,
